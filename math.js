38a919fdafe0a579e87332def5eeaf8b290cc367
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-01-31 21:11:26 +0600

    math function added

---
+++ math.js
@@ -0,0 +1,16 @@
+// var myNumber = 7.777
+
+// console.log(Math.floor(myNumber))
+// console.log(Math.ceil(myNumber))
+
+// console.log(Math.round(myNumber))
+
+
+
+for (let i = 0; i < 20; i++) {
+    var myNumber = Math.round(Math.random() * 6)
+
+    console.log(myNumber)
+
+
+}
