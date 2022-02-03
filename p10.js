539f464126948790adee9a1315b819d2be227117
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-02-03 14:58:48 +0600

    more problem solved

---
+++ p10.js
@@ -0,0 +1,7 @@
+let array = [10, 11, 13, 15, 66, 67, 77, 88, 89, 97, 99, 95, 94, 88]
+
+array.forEach(function(element) {
+
+    if (element > 80) { console.log(element) }
+
+})
---
+++ p11.js
@@ -0,0 +1,10 @@
+function calculate(num1, num2, num3) {
+
+    let result = num1 * num2 * num3
+
+    return result
+}
+
+let multiply = calculate(5, 8, 9)
+
+console.log(multiply)
